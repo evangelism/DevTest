b6232ee40ec39aea1d26bb842c27f4d756e022b9
Alexander Shapoval <ashapoms@hotmail.com>
2017-05-22 15:12:21 +0300

    Production parameters added

--- AzureRG/DevTestRG/open-wb-infra/DeployTemplate.ps1
+++ AzureRG/DevTestRG/open-wb-infra/DeployTemplate.ps1
@@ -1,6 +1,37 @@
-#
-# DeployTemplate.ps1
-#
+<#############################################################
+ #                                                           #
+ # DeployTemplate.ps1										 #
+ #                                                           #
+ #############################################################>
+
+<#
+ .Synopsis
+	The script creates a new resource group in Azure subscription and deploys resources based on 'openWb.json' template file and 'openWb.parameters.json' parameters file.
+
+ .Requirements
+	Azure PowerShell modules must be installed in order to run the script (https://www.microsoft.com/web/handlers/webpi.ashx/getinstaller/WindowsAzurePowershellGet.3f.3f.3fnew.appids).
+ .Parameter ResourceGroupLocation
+	Sets Azure region for the deployment. Default region is 'westeurope'.
+ .Parameter DeployIndex
+	Sets a number for the deployment iteration.
+ .Parameter ResourceGroupPrefix
+	Used to form resource group name and deployment name.  
+ .Parameter AzureUserName
+	Azure Active Directory tenant user name. This account is used to deploy all resources and should have necessary permissions. 
+ .Parameter AzureUserPassword
+	Azure Active Directory tenant password. 
+
+.Example
+     If no parameters are provided, default values are used.
+
+     .\DeployTemplate.ps1 
+
+.Example
+     This example creates ‘Open-RG02’ resource group in West Europe region and starts deployment with the name ‘Open-RG-Dep02’.
+
+     .\DeployTemplate.ps1 -ResourceGroupLocation 'westeurope' -DeployIndex '02' -ResourceGroupPrefix 'Open-RG' -AzureUserName 'admin@mytenant.onmicrosoft.com' -AzureUserPassword 'P@ssw0rd!@#$%' 
+#>
+
 
 Param(
 	[string] $ResourceGroupLocation = "westeurope",
@@ -10,19 +41,23 @@ Param(
 	[string] $AzureUserPassword = "DMSVonashu26.@3!"
 )
 
+# Prepare credentials and login to Azure subscription. 
 $AadPass = ConvertTo-SecureString $AzureUserPassword -AsPlainText -Force
 $AadCred = New-Object System.Management.Automation.PSCredential ($AzureUserName, $Aadpass)
 Login-AzureRmAccount -Credential $AadCred
 
+# Prepare environment variables.  
 $ResourceGroupName = $ResourceGroupPrefix + $DeployIndex
 $DeploymentName = $ResourceGroupPrefix + "-Dep" + $DeployIndex
-$TemplateFile = "C:\Users\ashapo\Work Folders\DPE\VS\OpenArm\open-wb\open-wb-infra\open-wb-infra\openWb.json"
-$TemplateParametersFile = "C:\Users\ashapo\Work Folders\DPE\VS\OpenArm\open-wb\open-wb-infra\open-wb-infra\openWb.parameters.json"
+$TemplateUri = "https://raw.githubusercontent.com/evangelism/DevTest/master/AzureRG/DevTestRG/open-wb-infra/openWb.json"
+$TemplateParameterUri = "https://raw.githubusercontent.com/evangelism/DevTest/master/AzureRG/DevTestRG/open-wb-infra/openWb.parameters.json"
 
+# Create a new resource group in given region.  
 New-AzureRmResourceGroup -Name $ResourceGroupName -Location $ResourceGroupLocation -Verbose -Force
 
+# Start a new deployment in created resource group. 
 New-AzureRmResourceGroupDeployment -Name $DeploymentName `
                                        -ResourceGroupName $ResourceGroupName `
-                                       -TemplateFile $TemplateFile `
-                                       -TemplateParameterFile $TemplateParametersFile `
+                                       -TemplateUri $TemplateUri `
+                                       -TemplateParameterUri $TemplateParameterUri `
                                        -Verbose `
