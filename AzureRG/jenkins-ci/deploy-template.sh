281ecc98dd57a1484877e2874521c57639817fbf
Emin Askerov <emin.askerov@gmail.com>
2017-06-08 18:26:34 +0300

    Enable azure login and other fixes

--- AzureRG/jenkins-ci/deploy-template.sh
+++ AzureRG/jenkins-ci/deploy-template.sh
@@ -1,26 +1,29 @@
 #!/bin/bash
+# Script usage example:
+# $ ./deploy-template.sh <azure_username> <azure_user_password>
+#
 
-# if [[ !("$#" -eq 2) ]]; 
-#     then echo "Parameters are missing!" >&2
-#     exit 1
-# fi
+if [[ !("$#" -eq 2) ]]; 
+    then echo "Parameters are missing!" >&2
+    exit 1
+fi
 
 #Prepare parameters  
+azure_username=$1
+azure_user_password=$2
+
 resource_group_location="westeurope"
 deploy_index="01"
 resource_group_prefix="Open-RG"
-azure_username=$1
-azure_user_password=$2
 
 #Prepare environment variables  
 resource_group_name="${resource_group_prefix}${deploy_index}"
 deployment_name="${resource_group_prefix}-Dep${deploy_index}"
-#template_uri="https://raw.githubusercontent.com/evangelism/DevTest/master/AzureRG/DevTestRG/open-wb-infra/openWb.json"
-#template_parameter_uri="https://raw.githubusercontent.com/evangelism/DevTest/master/AzureRG/DevTestRG/open-wb-infra/openWb.parameters.json"
 template_file="../DevTestRG/open-wb-infra/openWb.json"
 template_parameter_file="../DevTestRG/open-wb-infra/openWb.parameters.json"
 
-#az login --username $azure_username --password $azure_user_password
+# Azure login with provided username and password
+az login --username $azure_username --password $azure_user_password
 
 echo "Creating resource group: $resource_group_name"
 az group create \
@@ -28,7 +31,6 @@ az group create \
         --location $resource_group_location
 
 echo "Creating resource group: $resource_group_name"
-echo "Please wait..."
 az group deployment create \
         --name $deployment_name \
         --resource-group $resource_group_name \
